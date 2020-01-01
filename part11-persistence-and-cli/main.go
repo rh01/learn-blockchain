76682a28bdf61a3f22f8dade29f179e604b3db73
rh01 <1048157315@qq.com>
2020-01-01 22:23:42 +0800

    add part10, persistence blc to db

--- part11-persistence-and-cli/main.go
+++ part11-persistence-and-cli/main.go
@@ -1,5 +1,19 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"github.com/rh01/learn-blockchain/part11-persistence-and-cli/BLC"
+)
+
+func main() {
+	blockchain := BLC.NewBlockChain()
+	fmt.Println(blockchain)
+
+	fmt.Printf("Tip: %x\n", blockchain.Tip)
+
+	blockchain.AddBlock("XiaoMing Send to Xiaohua 20 BTC")
+	// fmt.Println(blockchain)
+
+	fmt.Printf("Tip: %x\n", blockchain.Tip)
 }

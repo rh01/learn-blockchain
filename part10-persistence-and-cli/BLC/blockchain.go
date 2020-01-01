622c62144cc1c49f18a762d18fbce269e0f7ca8f
rh01 <1048157315@qq.com>
2020-01-01 21:59:12 +0800

    add part10, persistence blc to db

--- part10-persistence-and-cli/BLC/blockchain.go
+++ part10-persistence-and-cli/BLC/blockchain.go
@@ -1,4 +1,4 @@
-package blc
+package BLC
 
 import (
 	"fmt"
--- part10-persistence-and-cli/main.go
+++ part10-persistence-and-cli/main.go
@@ -3,11 +3,11 @@ package main
 import (
 	"fmt"
 
-	"github.com/rh01/learn-blockchain/part10-persistence-and-cli/blc"
+	"github.com/rh01/learn-blockchain/part10-persistence-and-cli/BLC"
 )
 
 func main() {
-	blockchain := blc.NewBlockChain()
+	blockchain := BLC.NewBlockChain()
 	fmt.Println(blockchain)
 
 	fmt.Printf("Tip: %x\n", blockchain.Tip)

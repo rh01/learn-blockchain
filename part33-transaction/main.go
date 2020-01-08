7cfd88c3cc72d117e5a0985ff0b95f92632fdcac
rh01 <1048157315@qq.com>
2020-01-08 12:43:07 +0800

    完整命令行工具.

---
+++ part33-transaction/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"github.com/rh01/learn-blockchain/part33-transaction/BLC"
+)
+
+func main() {
+	//// 创建区块链
+	//blockchain := BLC.NewBlockChain()
+
+	// 创建CLI对象
+	cli := BLC.CLI{}
+
+	// 调用CLI的RUn方法
+	cli.Run()
+}
